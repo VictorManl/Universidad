c9c89c2ec42f1c83da2c7f2238395619ab59171c
Victor <victormel2003@gmail.com>
2022-02-25 13:44:06 -0500

    Actualizacion

--- Apps/OfConvenio/views.py
+++ Apps/OfConvenio/views.py
@@ -258,6 +258,7 @@ class editarProyecto(UpdateView):
     def get_context_data(self, **kwargs):
         context = super(editarProyecto, self).get_context_data(**kwargs)
         context['page_title'] = self.page_title
+        return context
 
 
 class verProyecto(DetailView):
